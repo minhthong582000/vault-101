a27c5ed80371a15d0c6e134bb45f1634df13c6a7
thonglm2 <thonglm2@c0s-wrk09.dp.vng.vn>
2022-01-22 01:14:31 +0700

    Add 'Configuration as a code' example

---
+++ vault-provision copy/scripts/provision.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+shopt -s nullglob
+
+function provision() {
+  set +e
+  pushd "$1" > /dev/null
+  for f in $(ls "$1"/*.json); do
+    p="$1/${f%.json}"
+    echo "---"
+    echo "Provisioning $p"
+
+    curl \
+      --location \
+      --header "X-Vault-Token: ${VAULT_TOKEN}" \
+      --data @"${f}" \
+      "${VAULT_ADDR}/v1/${p}" \
+      -w "Response Status Code: %{http_code}\n"
+      
+  done
+  popd > /dev/null
+  set -e
+}
+
+echo "Verifying Vault is unsealed"
+vault status > /dev/null
+
+pushd data >/dev/null
+provision sys/auth
+provision sys/mounts
+provision sys/policy
+provision auth/userpass/users
+popd > /dev/null
